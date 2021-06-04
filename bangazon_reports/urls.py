f172e1d6078eb83e161a9b41379402f4abbac83d
sllittrell <stacey.littrell@gmail.com>
2021-06-04 11:16:51 -0500

    report created for completed orders

--- bangazon_reports/urls.py
+++ bangazon_reports/urls.py
@@ -1,4 +1,5 @@
   
+from bangazon_reports.views.orders.closed_orders import completed_order_list
 from bangazon_reports.views.products.inexpensive_report import inexpensive_product_list
 from bangazon_reports.views.products.expensive_report import expensive_product_list
 from bangazon_reports.views.products.inexpensive_report import inexpensive_product_list
@@ -10,4 +11,6 @@ urlpatterns = [
     path('reports/user_favorited', user_favorited_list),
     path('reports/expensive_report', expensive_product_list),
     path('reports/inexpensive_report', inexpensive_product_list),
+    path('reports/inexpensive_report', inexpensive_product_list),
+    path('reports/completed_orders_report', completed_order_list),
 ]
--- bangazon_reports/views/__init__.py
+++ bangazon_reports/views/__init__.py
@@ -1,4 +1,5 @@
 from .users.users_favorited import user_favorited_list
 from .products.expensive_report import expensive_product_list
 from .products.inexpensive_report import inexpensive_product_list
+from .orders.closed_orders import completed_order_list
 from .connections import Connection
